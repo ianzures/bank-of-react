dff41a9b086e62e82092e044e9e32e567c793b05
ianzures <i.anzures514@gmail.com>
2020-07-04 18:27:39 -0400

    Base files

---
+++ src/components/AccountBalance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AccountBalance extends React.Component{
+    render() {
+        return (
+            <div>Balance: {this.props.accountBalance}</div>
+        );
+    }
+}
+
+export default AccountBalance;
---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AccountBalance from './AccountBalance.js';
+import { Link } from 'react-router-dom';
+
+const test = {
+    paddingLeft : "50%"
+}
+
+class Home extends React.Component{
+    render() {
+        return (
+            <div>
+                <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/09/12/21/bank-of-england-EPA.jpg" alt="bank" />
+                <h1 style={test}>Bank of React</h1>
+
+                <Link to="/userProfile">User Profile</Link>
+                <br/>
+                <Link to="/login">Login</Link>
+
+                <AccountBalance accountBalance={this.props.accountBalance}/>
+            </div> 
+        )
+    }
+}
+
+export default Home;
