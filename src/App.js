9da18a92c56b79ef32263866ed2f46889b30987a
ianzures <i.anzures514@gmail.com>
2020-07-04 18:26:06 -0400

    Base file which implements login, Home, userProfile, and accountBalance components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import LogIn from './components/LogIn';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+            accountBalance: 14568.27,
+            currentUser: {
+                userName: 'bob_loblaw',
+                memberSince: '08/23/99',
+            }
+        }
+    }
+
+    mockLogIn = (logInInfo) =>{
+
+        /* Page would not load with this syntax. Decided instead to more explicitly set updatedUser to currentUser object.
+        const newUser = { ...this.state.currentUser };*/
+
+        const newUser ={
+            userName: this.state.currentUser.userName,
+            memberSince: this.state.currentUser.memberSince
+        };
+
+        newUser.userName = logInInfo.userName;
+
+        this.setState({ currentUser: newUser });
+    } 
+
+    render(){
+
+        const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
+        const UserProfileComponent = () =>
+            (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
+        );
+        const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} {...this.props} />);
+
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={HomeComponent} />
+                    <Route exact path="/userProfile" component={UserProfileComponent} />
+                    <Route exact path="/login" render={LogInComponent} />
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
