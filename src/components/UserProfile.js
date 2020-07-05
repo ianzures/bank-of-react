00e432a2f89b8d893806acae14e687df2d4b0011
ianzures <i.anzures514@gmail.com>
2020-07-05 16:01:37 -0400

    Styled similar to homescreen

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+const links = {
+    float: 'right',
+    paddingRight: '3%',
+    fontSize: '30%',
+    fontWeight: 'normal',
+    fontFamily: 'Georgia'
+}
+
+const name = {
+    paddingLeft: "2%",
+    fontSize: '300%',
+}
+
+const background = {
+    backgroundImage: 'url("https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/09/12/21/bank-of-england-EPA.jpg")',
+    backgroundSize: 'cover',
+    backgroundPositin: 'center',
+    paddingBottom: '50%',
+    marginTop: '-.5%',
+}
+
+const heading = {
+    color: '#FFFAFA',
+    textShadow: '2px 2px 4px #000000',
+    textDecoration: 'underline',
+    fontSize: '100%',
+    fontWeight: 'normal',
+    paddingLeft: '5%',
+    paddingTop:'1%'
+}
+
 class UserProfile extends Component{
 
     render(){
         return (
             <div>
-                <h1>User Profile</h1>
+                <h1 style={name}>User Profile
+                    <span style={links}>
+                        <Link to="/">Home</Link>
+                    </span>
+                </h1>
 
-                <Link to="/">Home</Link>
-
-                <div>Username: {this.props.userName}</div>
-                <div>Member Since: {this.props.memberSince}</div>
+                <div style={background}>
+                    <div style={heading}>Username: {this.props.userName}</div>
+                    <div style={heading}>Member Since: {this.props.memberSince}</div>
+                </div>
+                
             </div>
         );
     }
