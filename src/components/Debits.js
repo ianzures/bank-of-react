d7b7b0a0f82d113fcc5b10c0957ce255bf2e96ab
ianzures <i.anzures514@gmail.com>
2020-07-05 11:01:25 -0400

    Swapped order of new transactions and api transactions (and used unshift() instead of push()) so that new transactions show up on top of the transactions display.

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -44,7 +44,7 @@ class Debits extends React.Component {
 
         let tempArr = [];
         this.state.transactions.map(tr => tempArr.push(tr));
-        tempArr.push(debitTransaction);
+        tempArr.unshift(debitTransaction);
 
         this.setState({ transactions: tempArr });
 
@@ -58,8 +58,8 @@ class Debits extends React.Component {
                 <h1>Debits</h1>
 
                 <div>
-                    {this.props.debits.map(deb => <p key={deb.id}>{deb.description}, {deb.amount}, {deb.date}</p>)}
                     {this.state.transactions.map(tr => <p key={tr.id}>{tr.description}, {tr.amount}, {tr.date}</p>)}
+                    {this.props.debits.map(deb => <p key={deb.id}>{deb.description}, {deb.amount}, {deb.date}</p>)}
                 </div>
                 <AccountBalance accountBalance={this.state.debAB} />
 
