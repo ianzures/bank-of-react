e72214d9cba58283c2852163eafcd754a4803b01
ianzures <i.anzures514@gmail.com>
2020-07-05 00:34:55 -0400

    Added ability to add debit transactions and have them displayed under previous transactions.

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -4,6 +4,52 @@ import AccountBalance from './AccountBalance'
 class Debits extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            debAB : this.props.AccountBalance,
+            transactions: [],
+            description: '',
+            amount: 0,
+            date:''
+        }
+    }
+
+    componentDidMount() {
+        let d = new Date();
+        let fullDate = d.getFullYear() + '-' + d.getDate() + '-' + (d.getMonth() + 1);
+        this.setState({ date: fullDate });
+    }
+
+    handleChange = (e) => {
+
+        if (e.target.name === 'des') {
+            this.setState({ description: e.target.value });
+        }
+
+        if (e.target.name === 'a') {
+            this.setState({ amount: e.target.value });
+        }
+
+    }
+
+    handleSubmit = event => {
+
+        event.preventDefault();
+        let mockId = 'cdsrcComp-' + this.state.description.substring(0, 4) + this.state.amount;
+        const debitTransaction = {
+            id: mockId,
+            description: this.state.description,
+            amount: this.state.amount,
+            date: this.state.date
+        }
+
+        let tempArr = [];
+        this.state.transactions.map(tr => tempArr.push(tr));
+        tempArr.push(debitTransaction);
+
+        this.setState({ transactions: tempArr });
+
+        let nTotal = this.props.accountBalance - this.state.amount;
+        this.setState({ accountBalance: nTotal });
     }
 
     render() {
@@ -13,9 +59,23 @@ class Debits extends React.Component {
 
                 <div>
                     {this.props.debits.map(deb => <p key={deb.id}>{deb.description}, {deb.amount}, {deb.date}</p>)}
+                    {this.state.transactions.map(tr => <p key={tr.id}>{tr.description}, {tr.amount}, {tr.date}</p>)}
                 </div>
+                <AccountBalance accountBalance={this.state.debAB} />
 
-                <AccountBalance accountBalance={this.props.accountBalance} />
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="description">Description</label>
+                        <input type="text" name="des" onChange={this.handleChange}/>
+                    </div>
+
+                    <div>
+                        <label htmlFor="amount">Amount</label>
+                        <input type="number" step="0.01" min="0" name="a" onChange={this.handleChange}/>
+                    </div>
+
+                    <button>Add debit</button>
+                </form>
 
             </div>   
         );
