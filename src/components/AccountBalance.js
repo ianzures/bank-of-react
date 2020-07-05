028056f849fba3f02a9322c832ba6ba1bcf47f65
ianzures <i.anzures514@gmail.com>
2020-07-05 15:47:33 -0400

    CSS: background image and heading added to Home page and AccountBalance now has a shadow

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+const bal = {
+    paddingLeft: '3%',
+    paddingTop: '2%',
+    textShadow: '2px 2px black'
+}
+const heading = {
+    color: 'white',
+    textShadow: '2px 2px 4px #000000',
+    textDecoration: 'underline',
+    fontSize: '150%',
+    fontWeight: 'normal'
+}
+
 class AccountBalance extends React.Component{
 
     render() {
         return (
-            <div>Balance: {this.props.accountBalance}</div>
+            <div style={bal}>
+                <p style={heading}>Balance: ${this.props.accountBalance}</p>
+            </div>
         );
     }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,26 +2,45 @@ import React from 'react';
 import AccountBalance from './AccountBalance.js';
 import { Link } from 'react-router-dom';
 
-const test = {
-    paddingLeft : "50%"
+const name = {
+    paddingLeft: "2%",
+    fontSize: '300%',
 }
+
+const links = {
+    paddingLeft: '50%',
+    fontSize: '30%',
+    fontWeight: 'normal',
+    fontFamily: 'Georgia'
+}
+const space = {
+    paddingRight: '1%',
+}
+
+const background = {
+    backgroundImage: 'url("https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/09/12/21/bank-of-england-EPA.jpg")',
+    backgroundSize: 'cover',
+    backgroundPositin: 'center',
+    paddingBottom: '50%',
+    marginTop: '-.5%',
+}
+
 
 class Home extends React.Component{
     render() {
         return (
-            <div>
-                <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/09/12/21/bank-of-england-EPA.jpg" alt="bank" />
-                <h1 style={test}>Bank of React</h1>
-
-                <Link to="/userProfile">User Profile</Link>
-                <br/>
-                <Link to="/login">Login</Link>
-                <br/>
-                <Link to="/debits">Debits</Link>
-                <br />
-                <Link to="/credits">Credits</Link>
-
-                <AccountBalance accountBalance={this.props.accountBalance}/>
+            <div >
+                <h1 style={name}>    Bank of React
+                    <span style={links}>
+                        <Link style={space} to="/userProfile">User Profile</Link>
+                        <Link style={space} to="/login">Login</Link>
+                        <Link style={space} to="/debits">Debits</Link>
+                        <Link style={space} to="/credits">Credits</Link>
+                    </span>
+                </h1>
+                <div style={background}>
+                    <AccountBalance accountBalance={this.props.accountBalance.toFixed(2)} />
+                </div>
             </div> 
         )
     }
